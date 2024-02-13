37af94a3ffb55927b5ff6af103099f79ddd2f447
Kayleen Carido <kvcarido@gmail.com>
2024-02-13 08:29:37 -0500

    completed aws_config

--- cloud-uploader.sh
+++ cloud-uploader.sh
@@ -4,11 +4,16 @@
 
 # Checks if AWS CLI is installed, asks user to authenticate
 aws_config () {
-    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-sudo installer -pkg AWSCLIV2.pkg -target /
-    echo "Please sign in to AWS CLI"
-    sleep 2
-    aws configure
+    if command -v aws &>/dev/null; then
+        aws configure
+    else
+        echo "AWS CLI not found – installing now..."
+        sleep 2
+        curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+        sudo installer -pkg ./AWSCLIV2.pkg -target /
+        sleep 2
+        echo "AWS CLI installed – please run command again."
+    fi
 }
 
-# aws_config
+aws_config
